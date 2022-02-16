2c90e92c2817d0bff99dc4e0ee17710663c4e504
minkyuu <minkyu023@gmail.com>
2022-02-16 23:04:25 +0900

    ps: programmers 220216

---
+++ programmers/PhoneBook.java
@@ -0,0 +1,13 @@
+// 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/42577 
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++){
+            if (phone_book[i + 1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
