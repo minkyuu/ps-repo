480c28d997dd5e7891335e452f7849196f8faeed
minkyuu <minkyu023@gmail.com>
2022-02-25 00:23:24 +0900

    ps: add solution at 220225

--- programmers/KthNum.java
+++ programmers/KthNum.java
@@ -29,3 +29,30 @@ class Solution {
         return temp[k];
     }
 }
+
+
+// 2022.02.25 풀이 추가
+// 그런데 이전에 풀이한게 더 깔끔해보인다...;;;;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1] - 1;
+            int k = commands[i][2] - 1;
+
+            List<Integer> temp = new ArrayList<>();
+            for (int j = start; j <= end; j++){
+                temp.add(array[j]);
+            }
+            Collections.sort(temp);
+            answer[i] = temp.get(k);
+        }
+
+        return answer;
+    }
+}
+
