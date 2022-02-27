06889364c83fe7a912d3d535c1e1025e725cffab
minkyuu <minkyu023@gmail.com>
2022-02-27 23:41:02 +0900

    ps: programmers 220227

---
+++ programmers/Divisor.java
@@ -0,0 +1,23 @@
+// 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/77884
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for (int i = left; i <= right; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count++;
+            }
+
+            if (count % 2 == 0){
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+}
